0ad2f21dad467d5248cf3065e74798114345480f
johnblackstar <alex.librando@gmail.com>
2023-04-27 00:56:08 +0900

    updated guess game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,11);
+     
+        Console.WriteLine("What is the Magic Number");
+        string userInput = Console.ReadLine();
+        int guess = -1;
+        
+
+        while (guess !=number)
+        {
+            Console.WriteLine("What is the Magic Number? ");
+            guess = int.Parse(Console.ReadLine());
+            
+
+
+
+            if ( guess < number)
+            {
+                Console.WriteLine ("Higher");
+            }
+                
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+    up            
+                Console.WriteLine("Congratulation you guessed it");
+            }
+        }    
+
     }
+    
 }
